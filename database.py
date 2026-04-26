58baaf5d195e7aed95135cbc77a9081958d7c6d1
tgardela <king_nothin@o2.pl>
2026-04-26 17:19:44 +0200

    Revert "Add Alembic migrations for database schema management"

--- database.py
+++ database.py
@@ -3,28 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-_engine = None
-_SessionLocal = None
-
-
-def get_engine():
-    global _engine
-    if _engine is None:
-        _engine = create_engine(os.environ["DATABASE_URL"])
-    return _engine
-
-
-def get_session_local():
-    global _SessionLocal
-    if _SessionLocal is None:
-        _SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
-    return _SessionLocal
-
-
 def get_db():
-    SessionLocal = get_session_local()
     db = SessionLocal()
     try:
         yield db
